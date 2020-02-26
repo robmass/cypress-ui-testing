1c9a4807b4ae88a97fba5f3521e8ed30047d63b9
Fredx87 <freddy87@gmail.com>
2020-02-26 19:34:14 +0100

    Added testing tools

--- images.js
+++ images.js
@@ -1,5 +1,6 @@
 import money from "./images/money.gif";
 import pyramid1 from "./images/testing-pyramid.png";
+import testingTools from "./images/testing-tools.png";
 import pyramid2 from "./images/testing-trophy.jpeg";
 import unitIntegration from "./images/unit-vs-integration-1.gif";
 
@@ -7,5 +8,6 @@ export const images = {
   money,
   pyramid1,
   pyramid2,
-  unitIntegration
+  unitIntegration,
+  testingTools
 };
