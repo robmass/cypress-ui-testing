77b3434abd9fa72e144d88c38d6555a2e740052b
Fredx87 <freddy87@gmail.com>
2020-02-26 17:00:07 +0100

    Downgraded mdx-deck to version 3.1

---
+++ images.js
@@ -0,0 +1,11 @@
+import money from "./images/money.gif";
+import pyramid1 from "./images/testing-pyramid.png";
+import pyramid2 from "./images/testing-trophy.jpeg";
+import unitIntegration from "./images/unit-vs-integration-1.gif";
+
+export const images = {
+  money,
+  pyramid1,
+  pyramid2,
+  unitIntegration
+};
