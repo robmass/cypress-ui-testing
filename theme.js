601237ec7cb284ad007a4949c6b3a06f7691a85f
Fredx87 <freddy87@gmail.com>
2020-04-29 11:44:29 +0200

    Added prism / vsDark syntax highlight

--- theme.js
+++ theme.js
@@ -1,8 +1,13 @@
+import vsDark from "react-syntax-highlighter/dist/cjs/styles/prism/vs-dark";
+
 export default {
   styles: {
     Slide: {
       justifyContent: "flex-start",
       padding: "0 60px"
     }
+  },
+  prism: {
+    style: vsDark
   }
 };
