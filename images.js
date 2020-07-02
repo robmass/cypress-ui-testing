30dff80b6f3be45294e5115861a063f7b1de25c8
Fredx87 <freddy87@gmail.com>
2020-07-02 17:31:22 +0200

    Refactored testing tools images

--- images.js
+++ images.js
@@ -2,6 +2,7 @@ import chromeRequest from "./images/chrome-request.png";
 import force from "./images/force.gif";
 import money from "./images/money.gif";
 import pyramid1 from "./images/testing-pyramid.png";
+import testingToolsCypress from "./images/testing-tools-cypress.png";
 import testingTools from "./images/testing-tools.png";
 import pyramid2 from "./images/testing-trophy.jpeg";
 import unitIntegration from "./images/unit-vs-integration-1.gif";
@@ -12,6 +13,7 @@ export const images = {
   pyramid2,
   unitIntegration,
   testingTools,
+  testingToolsCypress,
   force,
   chromeRequest
 };
