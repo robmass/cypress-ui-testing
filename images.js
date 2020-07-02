c10af627f4f658525136e95224403e88039f69ab
Fredx87 <freddy87@gmail.com>
2020-07-02 19:17:07 +0200

    Introduction refactoring

--- images.js
+++ images.js
@@ -1,6 +1,7 @@
 import chromeRequest from "./images/chrome-request.png";
 import force from "./images/force.gif";
 import money from "./images/money.gif";
+import testCurve from "./images/test-curve.jpg";
 import pyramid1 from "./images/testing-pyramid.png";
 import testingToolsCypress from "./images/testing-tools-cypress.png";
 import testingTools from "./images/testing-tools.png";
@@ -9,6 +10,7 @@ import unitIntegration from "./images/unit-vs-integration-1.gif";
 
 export const images = {
   money,
+  testCurve,
   pyramid1,
   pyramid2,
   unitIntegration,
