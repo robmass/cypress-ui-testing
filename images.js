ec7457c4894a02d06ca6eca482df9a250df66eb1
robmass <robertomassimini@hotmail.com>
2021-01-25 09:10:27 +0100

    images changed

--- images.js
+++ images.js
@@ -1,28 +1,20 @@
-import chromeRequest from "./images/chrome-request.png";
 import creator from "./images/creator.png";
-import force from "./images/force.gif";
 import homepage from "./images/homepage.png";
 import itinerary from "./images/itinerary.png";
-import money from "./images/money.gif";
 import slack from "./images/slack.png";
 import smartphone from "./images/smartphone-dialog.png";
 import testCurve from "./images/test-curve.jpg";
 import pyramid1 from "./images/testing-pyramid.png";
-import testingToolsCypress from "./images/testing-tools-cypress.png";
 import testingTools from "./images/testing-tools.png";
 import pyramid2 from "./images/testing-trophy.jpeg";
 import unitIntegration from "./images/unit-vs-integration-1.gif";
 
 export const images = {
-  money,
   testCurve,
   pyramid1,
   pyramid2,
   unitIntegration,
   testingTools,
-  testingToolsCypress,
-  force,
-  chromeRequest,
   smartphone,
   creator,
   homepage,
