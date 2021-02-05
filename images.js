5bd8d58058922fb7ba9541a0018d99fe35ba687d
robmass <robertomassimini@hotmail.com>
2021-02-05 10:34:01 +0100

    first image

--- images.js
+++ images.js
@@ -1,12 +1,16 @@
 import creator from "./images/creator.png";
+import demotime from "./images/demotime.gif";
 import homepage from "./images/homepage.png";
 import itinerary from "./images/itinerary.png";
+import questions from "./images/questions2.gif";
 import slack from "./images/slack.png";
 import smartphone from "./images/smartphone-dialog.png";
 import testCurve from "./images/test-curve.jpg";
 import pyramid1 from "./images/testing-pyramid.png";
 import testingTools from "./images/testing-tools.png";
 import pyramid2 from "./images/testing-trophy.jpeg";
+import thankyou from "./images/thankyou.gif";
+import uiTesting from "./images/ui-testing.jpg";
 import unitIntegration from "./images/unit-vs-integration-1.gif";
 
 export const images = {
@@ -20,4 +24,8 @@ export const images = {
   homepage,
   itinerary,
   slack,
+  questions,
+  thankyou,
+  demotime,
+  uiTesting,
 };
